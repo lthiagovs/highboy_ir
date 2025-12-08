0a21b49b267e0a04626492af514556ba6920051a
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2025-12-07 22:32:53 -0300

    feat(ir_rx): receive signal

--- main/ir_rx.c
+++ main/ir_rx.c
@@ -13,6 +13,8 @@ void ir_rx_init() {
         .resolution_hz = 1 * 1000 * 1000,
         .mem_block_symbols = 64,
         .gpio_num = IR_RX_GPIO,
+
+ 
         .flags.invert_in = IR_INVERT_IN,
         .flags.with_dma = false
     };
@@ -22,9 +24,6 @@ void ir_rx_init() {
 
     //2 - CONFIG CARRIER
     ir_rx_carrier_config();
-
-    //3 - ENABLE RMT
-    ir_rx_enable();
 
 }
 
@@ -63,4 +62,34 @@ void ir_rx_disable(){
         rx_enabled = false;
     }
 
+}
+
+//RECEIVE - FALTA FAZER O CALLBACK!!!
+rmt_symbol_word_t* ir_rx_receive(){
+
+    ir_rx_enable();
+
+    if(!rx_enabled) return NULL;
+
+    rmt_symbol_word_t raw_signal[64];
+
+    //CONFIG RECEIVE
+    rmt_receive_config_t receive_config = {
+        .signal_range_min_ns = 1250, 
+        .signal_range_max_ns = 12000000,
+    };
+
+    //START RECEIVE
+    ESP_ERROR_CHECK(rmt_receive(rx_chan, raw_signal, sizeof(raw_signal), &receive_config));
+
+    ESP_LOGW("IR RX:", "SINAL RECEBIDO");
+
+    size_t size_signal = sizeof(raw_signal) / sizeof(rmt_symbol_word_t);
+
+    print_signal(raw_signal, size_signal);
+
+    ir_rx_disable();
+
+    return raw_signal;
+    
 }
--- main/ir_rx.h
+++ main/ir_rx.h
@@ -1,6 +1,8 @@
 #ifndef IR_RX_H
 #define IR_RX_H
 
+#include <driver/rmt_types.h>
+
 //manel
 void ir_rx_init();
 void ir_rx_delete();
@@ -8,4 +10,6 @@ void ir_rx_carrier_config();
 void ir_rx_enable();
 void ir_rx_disable();
 
-#endif IR_RX_H
+rmt_symbol_word_t* ir_rx_receive();
+
+#endif
