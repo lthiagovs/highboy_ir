4f2c9a52ac90b09970fa444b7bc54e2f094490c6
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2025-12-09 18:55:34 -0300

    fix(ir_protocol_nec): nec_parse_frame function

--- main/ir_protocol_nec.c
+++ main/ir_protocol_nec.c
@@ -1,4 +1,6 @@
 #include <esp_log.h>
+#include <stdio.h>
+#include <string.h>
 #include "ir_protocol.h"
 #include "ir_protocol_nec.h"
 
@@ -22,38 +24,47 @@ bool nec_validate_leading_code(rmt_symbol_word_t *raw_data){
 IR_DATA* nec_parse_frame(rmt_symbol_word_t *raw_data){
 
     rmt_symbol_word_t *cur = raw_data;
-    IR_DATA nec_frame;
-    nec_frame.PROTOCOL = IR_PROTOCOL_NEC;
-    nec_frame.ADDRESS = 0;
-    nec_frame.COMMAND = 0;
-    nec_frame.REPEAT_FRAME = false;
+    IR_DATA* nec_frame = malloc(sizeof(IR_DATA));
+    if(!nec_frame) return NULL;
+
+    memset(nec_frame, 0, sizeof(IR_DATA));
+
+    nec_frame->PROTOCOL = IR_PROTOCOL_NEC;
+    nec_frame->ADDRESS = 0;
+    nec_frame->COMMAND = 0;
+    nec_frame->REPEAT_FRAME = false;
+    snprintf(nec_frame->NAME, sizeof(nec_frame->NAME), "NEC_FRAME");
+
+    if(!nec_validate_leading_code(raw_data)){
+        free(nec_frame);
+        return NULL;
+    }
 
     cur++;
-
-    if(!nec_validate_leading_code(raw_data)) return NULL;
-
     for (int i = 0; i < 16; i++) {
         if (nec_parse_logic1(cur)) {
-            nec_frame.ADDRESS |= 1 << i;
+            nec_frame->ADDRESS |= 1 << i;
         } else if (nec_parse_logic0(cur)) {
-            nec_frame.ADDRESS &= ~(1 << i);
+            nec_frame->ADDRESS &= ~(1 << i);
         } else {
-            return false;
+            //free(nec_frame);
+            return NULL;
         }
         cur++;
     }
 
     for (int i = 0; i < 16; i++) {
         if (nec_parse_logic1(cur)) {
-            nec_frame.COMMAND |= 1 << i;
+            nec_frame->COMMAND |= 1 << i;
         } else if (nec_parse_logic0(cur)) {
-            nec_frame.COMMAND &= ~(1 << i);
+            nec_frame->COMMAND &= ~(1 << i);
         } else {
-            return false;
+            //free(nec_frame);
+            return NULL;
         }
         cur++;
     }
 
-    return &nec_frame;
+    return nec_frame;
 
 }
