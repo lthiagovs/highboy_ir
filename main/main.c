9e5db41b5b8b45c59a11cf144322225f903e31f8
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2025-12-09 18:55:55 -0300

    add: rx example in main.c

--- main/main.c
+++ main/main.c
@@ -1,5 +1,18 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <esp_log.h>
+#include "ir_rx.h"
 
+int app_main(){
 
-void main(){
+    ir_rx_init();
+
+    while (1) {
+        ir_rx_receive();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ESP_LOGI("CLEAR", "\e[1;1H\e[2J");
+    }
+
+    return 0;
 
 }
